ead4a4ba63443a6adef322e81e7bddbde862de96
JohnPinilla <jpinillacelis@gmail.com>
2023-05-02 23:12:29 -0500

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeTwoComponent } from './components/pages/home-two/home-two.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
 const routes: Routes = [
-    {path: '', component: HomeTwoComponent},
-    {path: 'home-2', component: HomeTwoComponent},
+    {path: '', pathMatch: 'full', redirectTo: '/home-2'},
+    { 
+        path: 'home-2', 
+        component: HomeTwoComponent,
+       ... canActivate(() => redirectUnauthorizedTo(['/register']))
+    },
+    { path: 'register', component: RegisterComponent},
+    { path: 'login', component: LoginComponent },
     // Here add new pages component
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
